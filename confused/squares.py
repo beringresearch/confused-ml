348f345623854f3e0130ea1a19ea031d9915da53
Ignat Drozdov <idrozdov@beringresearch.com>
2019-02-08 09:18:15 +0000

    in-place histogram

--- confused/squares.py
+++ confused/squares.py
@@ -12,15 +12,21 @@ def squares(proba, actual, predicted):
     df.columns = df.columns.astype(str)
     
     df = df.melt(id_vars=['predicted', 'actual'])
-    alt.data_transformers.enable('default', max_rows=None)
-    chart = alt.Chart(df).mark_bar().encode(
-        x=alt.X('count(value)', axis=alt.Axis(
+
+    bins = [b/10 for b in range(11)]
+    df['bins'] = pd.cut(df['value'], bins=bins, include_lowest=True)
+    b = pd.DataFrame({'count' : df.groupby( [ 'bins', 'variable', 'actual', 'predicted'] )['bins'].size()}).reset_index()
+    b['bins'] = b['bins'].astype(str)
+
+
+    chart = alt.Chart(b, title='').mark_bar().encode(
+            x=alt.X('count', axis=alt.Axis(
                                    ticks=False,
                                    labels=False,
                                    title='')),
-        y=alt.Y('value', bin=alt.Bin(maxbins=10), title='Prediction Score'),
-        column='actual',
-        color='predicted:N',
-        tooltip=['count(predicted)', 'predicted']).properties(width=200).configure_axis(grid=False).configure_view(strokeOpacity=0)
+            y=alt.Y('bins:N', title='Prediction Score', sort=alt.EncodingSortField(field='bins', op='sum', order='descending')),
+            column=alt.Column('actual', title=''),
+            color='predicted:N',
+            tooltip=['count', 'predicted']).properties(width=100).configure_axis(grid=False).configure_view(strokeOpacity=0)
 
     return chart

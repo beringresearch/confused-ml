691bd5c5abc9caf3b6e891a47a078b338281d4ca
Ignat Drozdov <idrozdov@beringresearch.com>
2019-02-15 22:29:33 +0000

    add ,

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="confused",
     version='0.0.1-dev',
-    packages=['confused']  
+    packages=['confused'],
     install_requires=[
         'altair',
         'pandas',
